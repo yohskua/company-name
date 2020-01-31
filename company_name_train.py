0d77067d73b97233cc5440bcbefa2616abe27fa3
yoh2292 <guillaume.ligner@gmail.com>
2020-01-31 12:11:53 +0100

    save on disk csv with predictions from every x epochs

--- company_name_train.py
+++ company_name_train.py
@@ -1,48 +1,70 @@
+import os
+import argparse
+
+import datetime
 import tensorflow as tf
 
 # enter graph mode, usually faster
 tf.compat.v1.disable_eager_execution()
+
 from rnn_vae import RnnVae
 import pandas as pd
 
-df = pd.read_csv("~/datasets/companies_sorted.csv", sep=",")
-df_tmp = df[df.industry.isin(["information technology and services", "internet"])]
-df_tmp = df_tmp[df_tmp["current employee estimate"] > 5]
-df_tmp.dropna(subset=["name"], inplace=True)
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument("--epochs", default=1, type=int, help="")
+    argparser.add_argument("--batch-size", default=16, type=int, help="")
+    argparser.add_argument("--latent-dim", default=128, type=int, help="")
+    argparser.add_argument("--intermediate-dim", default=256, type=int, help="")
+    args = argparser.parse_args()
 
+    df = pd.read_csv("~/datasets/companies_sorted.csv", sep=",", nrows=100)
+    df_tmp = df[df.industry.isin(["information technology and services", "internet"])]
+    df_tmp = df_tmp[df_tmp["current employee estimate"] > 5]
+    df_tmp.dropna(subset=["name"], inplace=True)
 
-def allow_name(name):
-    for token in name:
-        try:
-            token.encode("ascii")
-        except:
-            return False
-    return True
+    def allow_name(name):
+        for token in name:
+            try:
+                token.encode("ascii")
+            except:
+                return False
+        return True
 
+    # remove chinese names and others that use characters we do not want to use
+    df_tmp["name_allowed"] = [allow_name(name) for name in df_tmp.name]
+    df_tmp = df_tmp[df_tmp["name_allowed"] == True]
 
-# remove chinese names and others that use characters we do not want to use
-df_tmp["name_allowed"] = [allow_name(name) for name in df_tmp.name]
-df_tmp = df_tmp[df_tmp["name_allowed"] == True]
+    df_tmp["len_name"] = [len(name) for name in df_tmp.name]
+    # remove too long names, hard to encode and we are not interested in them
+    df_tmp = df_tmp[df_tmp.len_name <= 12]
 
-df_tmp["len_name"] = [len(name) for name in df_tmp.name]
-# remove too long names, hard to encode and we are not interested in them
-df_tmp = df_tmp[df_tmp.len_name <= 25]
+    X = df_tmp.name.to_list()
+    rnn_vae = RnnVae(
+        X,
+        GO="A",
+        EOS="Z",
+        latent_dim=args.latent_dim,
+        intermediate_dim=args.intermediate_dim,
+    )
+    df_preds = pd.DataFrame()
+    frequence_saving = 2
+    number_predictions_epochs = 100
+    for i in range(10):
+        rnn_vae.train(epochs=1, batch_size=args.batch_size)
+        if (i + 1) % frequence_saving == 0:
+            df_preds["epoch_{}".format(i + 1)] = [
+                rnn_vae.sample_prediction() for _ in range(number_predictions_epochs)
+            ]
 
-X = df_tmp.name.to_list()
-X_dummy = ["google"] * 100
-rnn_vae = RnnVae(X, GO="A", EOS="Z", latent_dim=128, intermediate_dim=256)
-rnn_vae_dummy = RnnVae(X_dummy, GO="A", EOS="Z", latent_dim=128, intermediate_dim=256)
-rnn_vae.train(epochs=1, batch_size=16)
-rnn_vae_dummy.train(epochs=30, batch_size=16)
-
-# dummy demo
-word = "google"
-for _ in range(1000):
-    sampled_around = rnn_vae_dummy.sample_around(word)
-    if sampled_around != word:
-        print(sampled_around)
-
-# normal demo
-# problem is now we always sample around the same data point distribution: 0 mean and 1 std.
-for _ in range(30):
-    print(rnn_vae.sample_prediction())
+    current_date = datetime.datetime.now()
+    current_date = current_date.strftime("%Y-%m-%d-%H%M")
+    output_path = os.path.join(os.path.expanduser("~"), "tmp", current_date)
+    try:
+        os.makedirs(output_path)
+    except FileExistsError:
+        pass
+    except Exception as e:
+        print(e)
+        raise (e)
+    df_preds.to_csv(os.path.join(output_path, "predictions.csv"), index=False)
